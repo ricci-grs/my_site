f554ac4bd5275d899aa6039e622edbbe7ec3079a
ricci-grs <enricoriccidev@gmail.com>
2025-06-16 18:17:18 +0200

    add vercel for astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: vercel(),
   integrations: [tailwind(), react()],
   vite: {
     resolve: {
